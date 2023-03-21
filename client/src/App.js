8a8ea369da0487fe12ed125167005ad938af2370
Erik Anderson <108945295+ErikAndersonWebDev@users.noreply.github.com>
2023-03-20 22:00:36 -0700

    Structure dashboard page, start adding database values

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,40 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import Departments from "./components/Departments";
-import Items from "./components/Items/Items";
-import Folders from "./components/Folders";
-import Login from "./components/Login";
-import Logout from "./components/Logout";
-import Register from "./components/Register";
+import Departments from "./Components/Departments";
+import Items from "./Components/Items/Items";
+import Folders from "./Components/Folders";
+import Login from "./Components/Login";
+import Logout from "./Components/Logout";
+import Register from "./Components/Register";
+import Dashboard from "./Components/Dashboard";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-
 export default function App() {
-  const [state, setState] = useState({folders: [], departments: [], items: []});
+  const [state, setState] = useState({
+    folders: [],
+    departments: [],
+    items: [],
+  });
   useEffect(() => {
     Promise.all([
       axios.get("/api/folders"),
       axios.get("/api/departments"),
       axios.get("/api/items"),
-    ]).then((all) => {
-      const folders = all[0].data;
-      const departments = all[1].data;
-      const items = all[2].data;
-      console.log("promise return:", departments)
-      setState((prev) => ({ ...prev, folders, departments, items }));
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    ])
+      .then((all) => {
+        const folders = all[0].data;
+        const departments = all[1].data;
+        const items = all[2].data;
+        console.log("promise return:", departments);
+        setState((prev) => ({ ...prev, folders, departments, items }));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
 
-  console.log("state.department:", state.departments)
+  console.log("state.department:", state.departments);
 
   return (
     <BrowserRouter>
@@ -41,15 +46,32 @@ export default function App() {
         <Link to="/register">Register</Link>
       </nav>
       <main>
-      <Routes>
-        <Route path="/folders" element={<Folders folders={state.folders} />}/>
-        <Route path="/departments" element={<Departments departments={state.departments} />}/>
-        <Route path="/items" element={<Items items={state.items}/>}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/register" element={<Register />}/>
-        <Route path="/logout" element={<Logout />}/>
-      </Routes>
+        <Routes>
+          <Route
+            path="/dashboard"
+            element={
+              <Dashboard
+                items={state.items}
+                folders={state.folders}
+                departments={state.departments}
+              />
+            }
+          />
+          <Route
+            path="/folders"
+            element={<Folders folders={state.folders} />}
+          />
+          <Route
+            path="/departments"
+            element={<Departments departments={state.departments} />}
+          />
+          <Route path="/items" element={<Items items={state.items} />} />
+          <Route path="/items" element={<Items items={state.items} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
       </main>
     </BrowserRouter>
-  )
+  );
 }
--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import React from "react";
+import "./Dashboard.css";
+import axios from "axios";
 
-function Dashboard() {
+function Dashboard(props) {
+  const itemCount = props.items.length;
+  const departmentsCount = props.departments.length;
+  let qtyCount = 0;
+  let totalValueCount = 0;
+  props.items.map((item) => {
+    qtyCount += item.quantity;
+    totalValueCount += item.price_cents;
+  });
+  let departmentInfo = {};
+  axios
+    .get("http://localhost:3000/api/items")
+    .then((response) => {
+      console.log(response.data[2]);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
   return (
-    <div>
+    <section>
       <h1>Dashboard</h1>
-    </div>
+      <div className="boxone">
+        <div className="inventory">
+          <span> Sum of items: {itemCount}</span>
+          <span> Sum of departments: {departmentsCount}</span>
+          <span> Sum of Quantity: {qtyCount}</span>
+          <span> Sum of Total Value: ${totalValueCount / 100}</span>
+        </div>
+        <div className="new">
+          <div>click me</div>
+          <div>new button</div>
+        </div>
+      </div>
+      <div className="boxtwo">
+        <div>
+          Departments
+          <div className="departments">
+            {props.departments.map((item) => {
+              return (
+                <div className="department">
+                  <span>{item.name} </span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="boxthree">
+        <div>
+          Stock Levels At or Below Min Level
+          <div className="minlevels">
+            <div className="test">test</div>
+            <div className="test">test</div>
+            <div className="test">test</div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
